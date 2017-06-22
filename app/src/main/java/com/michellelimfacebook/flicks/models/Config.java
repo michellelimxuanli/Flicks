fcdda91c5111779fcab2d8f6b36410043b87bfa4
michellelimxuanli <michelle.lim@yale.edu>
2017-06-22 14:03:57 -0700

    Configured landscope mode

--- app/src/main/java/com/michellelimfacebook/flicks/models/Config.java
+++ app/src/main/java/com/michellelimfacebook/flicks/models/Config.java
@@ -13,14 +13,19 @@ public class Config {
     String imageBaseUrl;
     //the poster size to use when fetching images, part of the url
     String posterSize;
+    //the backdrop size to use when fetching images
+    String backdropSize;
 
     public Config(JSONObject object) throws JSONException {
         JSONObject images = object.getJSONObject("images");
         //get the image base Url
         imageBaseUrl = images.getString("secure_base_url");
-        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         // use option at index 3 or w342 as a fallback
+        JSONArray posterSizeOptions = images.getJSONArray("poster_sizes");
         posterSize = posterSizeOptions.optString(3, "w342");
+        //parse the backdrop sizes and use the option at index 1 or w780 as a fallback
+        JSONArray backdropSizeOptions = images.getJSONArray("backdrop_sizes");
+        backdropSize = backdropSizeOptions.optString(1, "w780");
     }
 
     public String getImageUrl(String size, String path){
@@ -33,5 +38,9 @@ public class Config {
 
     public String getPosterSize() {
         return posterSize;
+    }
+
+    public String getBackdropSize() {
+        return backdropSize;
     }
 }
